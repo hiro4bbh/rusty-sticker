a7bc708aa3a85cb6f001d4ddb5094533b2726b65
Tatsuhiro Aoshima <hiro4bbh@gmail.com>
2018-03-13 13:00:41 +0900

    Fix soft indents

--- src/bin/main_benchmarks.rs
+++ src/bin/main_benchmarks.rs
@@ -9,89 +9,89 @@ extern crate rand;
 use rand::Rng;
 
 fn hash_u32(x: u32) -> u32 {
-	x*2654435761
+    x*2654435761
 }
 
 fn sort_largests_with_heap(kcs: &mut [(u32, u32)], K: usize) {
-	// This implementation comes from http://web.archive.org/web/20140807181610/http://fallabs.com/blog-ja/promenade.cgi?id=104.
-	// Strategy:
-	//   Basically, this is an in-place heap sort.
-	//   Retain the heap at the first whose size is at most K for efficiently sorting the K largest counts.
-	let K = K.min(kcs.len());
-	// First, the only first entry is automatically in the heap.
-	let mut cur = 1;
-	// The first K entries are inserted into the heap.
-	// Any entry in the heap satisfies that the entry is "NOT LARGER" than any descendants of it.
-	// The order in the heap are reversed at the third step.
-	// The heap structure is retained as follows:
-	//   [0] -> [1] -> [3] -> ...
-	//              -> [4] -> ...
-	//       -> [2] -> [5] -> ...
-	//              -> [6] -> ...
-	// Hence, prev(k) = (k - 1)/2, left(k) = 2*k + 1, and right(k) = 2*k + 2.
-	while cur < K {
-		// Insert the cur-th entry into the heap.
-		let mut cidx = cur;
-		while cidx > 0 {
-			let pidx = (cidx - 1) / 2; // prev(cidx)
-			if !(kcs[pidx].1 > kcs[cidx].1) {
-				break
-			}
-			// Swap the current entry with the parent one, because the current one is smaller.
+    // This implementation comes from http://web.archive.org/web/20140807181610/http://fallabs.com/blog-ja/promenade.cgi?id=104.
+    // Strategy:
+    //   Basically, this is an in-place heap sort.
+    //   Retain the heap at the first whose size is at most K for efficiently sorting the K largest counts.
+    let K = K.min(kcs.len());
+    // First, the only first entry is automatically in the heap.
+    let mut cur = 1;
+    // The first K entries are inserted into the heap.
+    // Any entry in the heap satisfies that the entry is "NOT LARGER" than any descendants of it.
+    // The order in the heap are reversed at the third step.
+    // The heap structure is retained as follows:
+    //   [0] -> [1] -> [3] -> ...
+    //              -> [4] -> ...
+    //       -> [2] -> [5] -> ...
+    //              -> [6] -> ...
+    // Hence, prev(k) = (k - 1)/2, left(k) = 2*k + 1, and right(k) = 2*k + 2.
+    while cur < K {
+        // Insert the cur-th entry into the heap.
+        let mut cidx = cur;
+        while cidx > 0 {
+            let pidx = (cidx - 1) / 2; // prev(cidx)
+            if !(kcs[pidx].1 > kcs[cidx].1) {
+                break
+            }
+            // Swap the current entry with the parent one, because the current one is smaller.
             kcs.swap(cidx, pidx);
-			// Perform this recursively at the parent entry.
-			cidx = pidx
-		}
+            // Perform this recursively at the parent entry.
+            cidx = pidx
+        }
         cur += 1
-	}
-	// Second, the remain entries are inserted into the heap as keeping the heap size is k.
-	while cur < kcs.len() {
-		// Insert the current entry if it is larger than the smallest one in the heap.
-		if kcs[cur].1 > kcs[0].1 {
-			// Procedure A: Insert the current entry into the size K heap.
-			// Swap the current entry and the smallest one in the heap.
+    }
+    // Second, the remain entries are inserted into the heap as keeping the heap size is k.
+    while cur < kcs.len() {
+        // Insert the current entry if it is larger than the smallest one in the heap.
+        if kcs[cur].1 > kcs[0].1 {
+            // Procedure A: Insert the current entry into the size K heap.
+            // Swap the current entry and the smallest one in the heap.
             kcs.swap(0, cur);
-			// Insert the current entry in the heap.
-			let (mut pidx, bot) = (0, K/2);
-			while pidx < bot {
-				// Take the smaller child as the current entry.
-				let mut cidx = 2*pidx + 1; // left(cidx)
-				if cidx < K - 1 && kcs[cidx].1 > kcs[cidx+1].1 {
-					cidx += 1;
-				}
-				if kcs[cidx].1 > kcs[pidx].1 {
-					break
-				}
-				// Swap the current entry with the selected child, because the current one is larger.
+            // Insert the current entry in the heap.
+            let (mut pidx, bot) = (0, K/2);
+            while pidx < bot {
+                // Take the smaller child as the current entry.
+                let mut cidx = 2*pidx + 1; // left(cidx)
+                if cidx < K - 1 && kcs[cidx].1 > kcs[cidx+1].1 {
+                    cidx += 1;
+                }
+                if kcs[cidx].1 > kcs[pidx].1 {
+                    break
+                }
+                // Swap the current entry with the selected child, because the current one is larger.
                 kcs.swap(pidx, cidx);
-				// Perform this recursively at the child entry.
-				pidx = cidx;
-			}
-		}
-		cur += 1;
-	}
-	// Third, the entries in the heap are reversed.
-	// This is achieved by shrinking the heap one by one:
-	//   Taking the largest entry as the current one, insert it to the heap.
-	// Hence, the largest entry is being selected as the current one as pushing down the smaller entries.
-	cur = K - 1;
-	while cur > 0 {
-		// Apply procedure A in the size cur heap.
+                // Perform this recursively at the child entry.
+                pidx = cidx;
+            }
+        }
+        cur += 1;
+    }
+    // Third, the entries in the heap are reversed.
+    // This is achieved by shrinking the heap one by one:
+    //   Taking the largest entry as the current one, insert it to the heap.
+    // Hence, the largest entry is being selected as the current one as pushing down the smaller entries.
+    cur = K - 1;
+    while cur > 0 {
+        // Apply procedure A in the size cur heap.
         kcs.swap(0, cur);
-		let (mut pidx, bot) = (0, cur/2);
-		while pidx < bot {
-			let mut cidx = 2*pidx + 1;
-			if cidx < cur-1 && kcs[cidx].1 > kcs[cidx+1].1 {
-				cidx += 1;
-			}
-			if kcs[cidx].1 > kcs[pidx].1 {
-				break
-			}
+        let (mut pidx, bot) = (0, cur/2);
+        while pidx < bot {
+            let mut cidx = 2*pidx + 1;
+            if cidx < cur-1 && kcs[cidx].1 > kcs[cidx+1].1 {
+                cidx += 1;
+            }
+            if kcs[cidx].1 > kcs[pidx].1 {
+                break
+            }
             kcs.swap(pidx, cidx);
-			pidx = cidx;
-		}
-		cur -= 1;
-	}
+            pidx = cidx;
+        }
+        cur -= 1;
+    }
 }
 
 struct HashMap32(Box<[(u32, u32)]>);
